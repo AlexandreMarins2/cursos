2bdb1e59d6e655e9355ddb44cae2a3efe3b2af75
AlexandreMarins2 <prosperdesigngrafica@outlook.com>
2018-06-22 13:01:27 -0300

    Aula atualizada

---
+++ curso_web_moderno/curso_js/exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,15 @@
+// Comentário de uma linha
+
+console.log('Linha 1')
+
+/*
+Comentário de
+múltiplas linha
+*/
+
+/*
+ * Comentário de
+ * várias linha
+ * com tópicos em asterísco colocados pela IDE.
+*/
+
---
+++ curso_web_moderno/curso_js/exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,9 @@
+console.log("Sentença de Código")
+
+
+{
+    {
+        console.log("Olá");
+        console.log('Senhor Jesus!') // Padrão do curso
+    }
+}
